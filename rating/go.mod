fea42828fcf1e12a36ac193408e0204ad4519055
takez0_o <takez0_o@protonmail.com>
2023-05-28 00:24:05 +0300

    .

---
+++ rating/go.mod
@@ -0,0 +1,24 @@
+module github.com/honestworkdao/honestwork-services/rating
+
+go 1.20
+
+require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.12.0 // indirect
+	github.com/getsentry/sentry-go v0.21.0 // indirect
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
+	github.com/holiman/uint256 v1.2.2-0.20230321075855-87b91420868c // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.11.6 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
---
+++ rating/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/getsentry/sentry-go"
+	"github.com/honestworkdao/honestwork-services/deal/abi/hwescrow"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RatingWatcher struct {
+	mc  *mongo.Client
+	rpc *ethclient.Client
+}
+
+func main() {
+	_ = sentry.Init(sentry.ClientOptions{
+		Dsn:              os.Getenv("SENTRY_DSN"),
+		Debug:            true,
+		AttachStacktrace: true,
+	})
+	defer sentry.Flush(2 * time.Second)
+	mongodb_client := NewMongoClient()
+	defer mongodb_client.Disconnect(context.Background())
+	rpc, err := ethclient.Dial(os.Getenv("ARBITRUM_RPC"))
+	if err != nil {
+		panic(err)
+	}
+	rw := NewRatingWatcher(mongodb_client, rpc)
+	rw.WatchRatings()
+}
+
+func NewRatingWatcher(mc *mongo.Client, rpc *ethclient.Client) *RatingWatcher {
+	return &RatingWatcher{
+		mc:  mc,
+		rpc: rpc,
+	}
+}
+
+func (r *RatingWatcher) WatchRatings() {
+	for {
+		r.fetchAllRatings()
+		time.Sleep(time.Duration(30) * time.Minute)
+	}
+}
+
+func (r *RatingWatcher) fetchAllRatings() {
+	listers := fetchAllListers()
+	members := fetchAllMembers()
+	for _, lister := range listers {
+		updateRating(lister)
+	}
+	for _, member := range members {
+		updateRating(member)
+	}
+}
+
+func search(length int, f func(index int) bool) int {
+	for index := 0; index < length; index++ {
+		if f(index) {
+			return index
+		}
+	}
+	return -1
+}
+
+func fetchAllListers() []string {
+	return []string{}
+}
+
+func fetchAllMembers() []string {
+	return []string{}
+}
+
+func updateRating(address string) {
+	rating := FetchAggregatedRating(address)
+	_ = rating
+}
+
+func FetchAggregatedRating(user_address string) float64 {
+	var client *ethclient.Client
+	client, err := ethclient.Dial(os.Getenv("ARBITRUM_RPC"))
+	if err != nil {
+		sentry.CaptureException(err)
+	}
+	defer client.Close()
+	escrow_address_hex := common.HexToAddress(os.Getenv("ESCROW_ADDRESS"))
+	instance, err := hwescrow.NewHwescrow(escrow_address_hex, client)
+	if err != nil {
+		sentry.CaptureException(err)
+	}
+	user_address_hex := common.HexToAddress(user_address)
+	rating, err := instance.GetAggregatedRating(nil, user_address_hex)
+	if err != nil {
+		sentry.CaptureException(err)
+	}
+	precision, err := instance.GetPrecision(nil)
+	if err != nil {
+		sentry.CaptureException(err)
+	}
+	rating_normalized := float64(rating.Int64()) / float64(precision.Int64())
+	return rating_normalized
+}
